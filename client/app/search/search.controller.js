a48022f43336a8004e4a11179649cef3aa66e316
Phil Machine <ficklephil@hotmail.com>
2015-02-14 20:39:15 +0000

    ThenHome rulez! Map

---
+++ client/app/search/search.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('thenHomeApp')
+  .controller('SearchCtrl', function ($scope, uiGmapGoogleMapApi) {
+    $scope.message = 'Hello';
+
+    uiGmapGoogleMapApi.then(function(maps){
+      console.log('Google maps is ready');
+      $scope.map = { center: { latitude: 51.4790383, longitude: -0.2271019 }, zoom: 11 };
+    });
+
+
+  });
---
+++ client/app/search/search.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('thenHomeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('search', {
+        url: '/search',
+        templateUrl: 'app/search/search.html',
+        controller: 'SearchCtrl'
+      });
+  });
+
+angular.module('thenHomeApp')
+  .config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      //    key: 'your api key',
+      v: '3.17',
+      libraries: 'weather,geometry,visualization'
+    });
+  });
